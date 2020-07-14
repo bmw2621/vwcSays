50619b7a576740cc6d34a77800ce8b179423e8dc
andrew-t-james <ajames20@gmail.com>
2020-07-14 11:28:34 -0500

    Fix url for lambda, add serverless offline

--- handler.js
+++ handler.js
@@ -1,9 +1,8 @@
-'use strict';
+// 'use strict';
 const Jimp = require('jimp');
 
 module.exports.jeromeSays = async (event, context, callback) => {
-
-  let url = 'https://s3.console.aws.amazon.com/s3/object/vwcsays/jerome/'
+  let url = 'https://vwcsays.s3.us-east-2.amazonaws.com/jerome/'
   const images = ['captainJS.jpeg','jeromeSays.jpeg','motivationSays.jpeg','musclesSays.jpeg']
   url = url + `${images[Math.floor(Math.random() * images.length)]}`
 
@@ -12,7 +11,7 @@ module.exports.jeromeSays = async (event, context, callback) => {
     maxWidth: 400, //image width - 10px margin left - 10px margin right
     maxHeight: 267, //logo height + margin
     placementX: 505, // 10px in on the x axis
-    placementY: 95 //bottom of the image: height - maxHeight - margin 
+    placementY: 95 //bottom of the image: height - maxHeight - margin
   };
 
   const image = await Jimp.read(url)
@@ -20,10 +19,10 @@ module.exports.jeromeSays = async (event, context, callback) => {
       Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => ([tpl, font]))
       ))
     .then(data => {
-      
+
       let tpl = data[0];
       let font = data[1];
-      
+
       return tpl.print(font, textData.placementX, textData.placementY, {
         text: textData.text,
         alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
@@ -32,20 +31,20 @@ module.exports.jeromeSays = async (event, context, callback) => {
     })
 
     const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
-    
-    return {
+
+  return {
       statusCode: 200,
       headers: {
         'content-type': 'image/jpeg'
       },
       body: buffer.toString('base64'),
       isBase64Encoded: true
-    };
+    }
 };
 
 module.exports.jodySays = async event => {
   const Jimp = require('jimp');
-  let url = 'https://s3.console.aws.amazon.com/s3/object/vwcsays/jody/'
+  let url = 'https://vwcsays.s3.us-east-2.amazonaws.com/jody/'
   const images = ['jodyDrill.jpg','jodyEyeroll.jpg','jodyHappy.jpg','jodyMad.jpg']
   url = url + `${images[Math.floor(Math.random() * images.length)]}`
   return {
