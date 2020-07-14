e07d7ef47fda7f0fe2bb138b03664b5e5a612dba
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2020-07-14 15:35:15 -0400

    Add jodySays functionality

--- handler.js
+++ handler.js
@@ -32,30 +32,63 @@ module.exports.jeromeSays = async (event, context, callback) => {
 
     const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
 
-  return {
+    callback(null, {
       statusCode: 200,
-      headers: {
-        'content-type': 'image/jpeg'
-      },
+      headers: { 'Content-Type': 'image/jpeg' },
       body: buffer.toString('base64'),
       isBase64Encoded: true
-    }
+    })
 };
 
 module.exports.jodySays = async event => {
   const Jimp = require('jimp');
   let url = 'https://vwcsays.s3.us-east-2.amazonaws.com/jody/'
-  const images = ['jodyDrill.jpg','jodyEyeroll.jpg','jodyHappy.jpg','jodyMad.jpg']
-  url = url + `${images[Math.floor(Math.random() * images.length)]}`
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        url: `${url}`,
-        message: event.queryStringParameters.phrase ? event.queryStringParameters.phrase : "Hello VWC"
-      },
-      null,
-      2
-    ),
+  let image;
+  switch(event.path.jodytype){
+    case 'drill':
+      image = 'jodyDrill.jpg';
+      break;
+    case 'eyeroll':
+      image = 'jodyEyeroll.jpg';
+      break;
+    case 'mad':
+      image = 'jodyMad.jpg';
+      break;
+    case 'happy':
+    default:
+      image = 'jodyHappy.jpg';
+  }
+  url = url + image
+  const textData = {
+    text: event.queryStringParameters.text ? event.queryStringParameters.text : 'Hello VWC', //the text to be rendered on the image
+    maxWidth: 400, //image width - 10px margin left - 10px margin right
+    maxHeight: 267, //logo height + margin
+    placementX: 505, // 10px in on the x axis
+    placementY: 95 //bottom of the image: height - maxHeight - margin
   };
+
+  const image = await Jimp.read(url)
+    .then(tpl => (
+      Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => ([tpl, font]))
+      ))
+    .then(data => {
+
+      let tpl = data[0];
+      let font = data[1];
+
+      return tpl.print(font, textData.placementX, textData.placementY, {
+        text: textData.text,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+      }, textData.maxWidth, textData.maxHeight);
+    })
+
+    const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+
+    callback(null, {
+      statusCode: 200,
+      headers: { 'Content-Type': 'image/jpeg' },
+      body: buffer.toString('base64'),
+      isBase64Encoded: true
+    })
 };
